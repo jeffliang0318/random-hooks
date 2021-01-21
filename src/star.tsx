f9b709b4cde7d3bf0f120a9b1aa482552e09bdf1
jeffliang0318 <liangnash0318@gmail.com>
2021-01-21 10:38:00 -0800

    Add wireframe stars review component

--- src/star.tsx
+++ src/star.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import { FaStar, FaRegStar } from 'react-icons/fa';
 import './star-review.css'
 function StarIcon () {
     return (
@@ -8,31 +8,75 @@ function StarIcon () {
         </span>
     )
 }
+function RegStar () {
+    return (
+        <span>
+            <FaRegStar/>
+        </span>
+    )
+}
 
-export default function StarsReview ({ starNumbers = 5 }) {
-    const array = [...new Array(5)]
+function Stars ({ starNumbers = 5, type = '' }) {
+    const array = [...new Array(starNumbers)]
+    return (
+        <>
+            {array.map((_, i) => {
+                return type === 'FaStar' ? <StarIcon key={i} /> : <RegStar key={i}/>
+            })}  
+        </>
+    )
+}
+
+export default function StarsReview () {
     return(
         <div className='d-flex justify-content-center align-items-center'>
-            <div className='rating'>
-                {
-                    array.map((_, i) => {
-                        return <StarIcon key={i} />
-                    })
-
-                }
+            <div className='rating-solid'>
+                <Stars type="FaStar"/>
             </div>
             <pre>
                 <code>
                     {
                         `
-                        .rating {
-                            direction: rtl;
-                            text-align: center;
+                        .rating,
+                        .rating-solid {
+                          direction: rtl;
+                          text-align: center;
                         }
 
+                        .rating-solid > span:hover,
+                        .rating-solid > span:hover ~ span {
+                          color: gold;
+                        }`
+                    }
+                </code>
+            </pre>
+            <div className='rating'>
+                <Stars/>
+            </div>
+            <pre>
+                <code>
+                    {
+                        `
+                        .rating,
+                        .rating-solid {
+                          direction: rtl;
+                          text-align: center;
+                        }
+                        .rating > span {
+                          display: inline-block;
+                          position: relative;
+                        }
                         .rating > span:hover,
                         .rating > span:hover ~ span {
-                            color: gold;
+                          color: transparent;
+                        }
+                        
+                        .rating > span:hover:before,
+                        .rating > span:hover ~ span:before {
+                          content: "\\2605";
+                          position: absolute;
+                          left: 0;
+                          color: gold;
                         }`
                     }
                 </code>
