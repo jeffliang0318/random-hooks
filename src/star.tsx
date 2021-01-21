e56075e00390944ee8574c4715d563be52a698df
jeffliang0318 <liangnash0318@gmail.com>
2021-01-21 12:27:19 -0800

    hook star

--- src/star.tsx
+++ src/star.tsx
@@ -1,86 +1,81 @@
-import React from 'react';
-import { FaStar, FaRegStar } from 'react-icons/fa';
 import './star-review.css'
-function StarIcon () {
-    return (
-        <span>
-            <FaStar/>
-        </span>
-    )
-}
-function RegStar () {
-    return (
-        <span>
-            <FaRegStar/>
-        </span>
-    )
-}
+import StarReview, { Stars } from './star-review'
 
-function Stars ({ starNumbers = 5, type = '' }) {
-    const array = [...new Array(starNumbers)]
+function wierFrameStars () {
     return (
         <>
-            {array.map((_, i) => {
-                return type === 'FaStar' ? <StarIcon key={i} /> : <RegStar key={i}/>
-            })}  
-        </>
-    )
-}
-
-export default function StarsReview () {
-    return(
-        <div className='d-flex justify-content-center align-items-center'>
-            <div className='rating-solid'>
-                <Stars type="FaStar"/>
-            </div>
-            <pre>
-                <code>
-                    {
-                        `
-                        .rating,
-                        .rating-solid {
-                          direction: rtl;
-                          text-align: center;
-                        }
-
-                        .rating-solid > span:hover,
-                        .rating-solid > span:hover ~ span {
-                          color: gold;
-                        }`
-                    }
-                </code>
-            </pre>
             <div className='rating'>
                 <Stars/>
             </div>
             <pre>
                 <code>
                     {
-                        `
-                        .rating,
-                        .rating-solid {
-                          direction: rtl;
-                          text-align: center;
-                        }
-                        .rating > span {
-                          display: inline-block;
-                          position: relative;
-                        }
-                        .rating > span:hover,
-                        .rating > span:hover ~ span {
-                          color: transparent;
-                        }
-                        
-                        .rating > span:hover:before,
-                        .rating > span:hover ~ span:before {
-                          content: "\\2605";
-                          position: absolute;
-                          left: 0;
-                          color: gold;
-                        }`
+`
+.rating,
+.rating-solid {
+    direction: rtl;
+    text-align: center;
+}
+.rating > span {
+    display: inline-block;
+    position: relative;
+}
+.rating > span:hover,
+.rating > span:hover ~ span {
+    color: transparent;
+}
+
+.rating > span:hover:before,
+.rating > span:hover ~ span:before {
+    content: "\\2605";
+    position: absolute;
+    left: 0;
+    color: gold;
+}`
                     }
                 </code>
             </pre>
+        </>
+    )
+}
+
+function solidStars () {
+    return (
+        <>
+            <div className='rating-solid'>
+                <Stars type="FaStar"/>
+            </div>
+            <pre>
+                <code>
+                    {
+`
+.rating,
+.rating-solid {
+    direction: rtl;
+    text-align: center;
+}
+
+.rating-solid > span:hover,
+.rating-solid > span:hover ~ span {
+    color: gold;
+}`
+                    }
+                </code>
+            </pre>
+        </>
+    )
+}
+
+export default function StarsReview () {
+    return(
+        <div className="container-md">
+            <h2>Hover only</h2>
+            <div className='d-flex justify-content-between align-items-center'>
+                { solidStars() }
+                { wierFrameStars() }
+            </div>
+            <h2>Click to set star review</h2>
+            <StarReview />
         </div>
 
     )
