e3b428ff7127e7802c74bbbb3c53ac4aca24fb7c
jeffliang0318 <liangnash0318@gmail.com>
2021-01-22 23:25:32 -0800

    Add code for referance

--- src/referance.tsx
+++ src/referance.tsx
@@ -5,7 +5,7 @@ const grecaptcha = {
   render: function(element: HTMLDivElement, { callback }: { callback: Function}) {
     if (isCalled) throw new Error("You can only call me once");
     isCalled = true;
-    element.innerText = "click me if you are not robot";
+    element.innerText = "click show current current callback function";
     element.addEventListener("click", function() {
       callback("you got token!");
     });
@@ -38,20 +38,52 @@ const ReCAPTCHA = ({ onChange }: {onChange: Function}) => {
 
 export default function Referance() {
   const [isOld, setIsOld] = useState(true);
-  const oldFunction = () => console.log("old function");
-  const newFunction = () => console.log("new function");
+  const pRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const oldFunction = () => {
+    pRef.current.innerText = 'old function';
+  };
+
+  const newFunction = () => {
+    pRef.current.innerText = 'new function';
+  }
 
   return (
-    <div className="">
+    <div className="container-md">
       <ReCAPTCHA onChange={isOld ? oldFunction : newFunction} />
-      <button
+      <p ref={pRef}>Click ^</p>
+      <button type="button" className="btn-primary"
         onClick={() => {
           console.log("Switch to new function");
           setIsOld(false);
         }}
       >
-        change function
+        replace with new function
       </button>
+      <pre><code className="hljs typescript">
+        {
+        `  const divRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+        const cbRef = useRef(onChange)
+        
+        const handleCallback = () => {
+          cbRef.current();
+        }
+      
+        useEffect(() => {
+          cbRef.current = onChange
+        }, [onChange])
+        
+        useEffect(() => {
+          const handleLoad = () => {
+            grecaptcha.render(divRef.current, {
+              callback: handleCallback
+            });
+          };
+          handleLoad();
+        }, []);
+      
+        return <div ref={divRef} />;`
+        }
+</code></pre>
     </div>
   );
 }
