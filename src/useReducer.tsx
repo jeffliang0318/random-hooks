f17292225444c7613e41cc40dcf8dfe18e994817
jeffliang0318 <liangnash0318@gmail.com>
2021-01-27 14:43:55 -0800

    Play with useRudecer

--- src/useReducer.tsx
+++ src/useReducer.tsx
@@ -2,11 +2,30 @@ import { useReducer } from 'react';
 
 export default function CaseUseReducer () {
     const [count, setCount] = useReducer((count: number, newCount: number) => { return count + newCount}, 0)
-
+    const [isCheck, toggle] = useReducer((isCheck: boolean) =>  !isCheck, false)
     return (
-        <div className="d-flex">
-            <p>{count}</p>
-            <button onClick={() => setCount(1)}>Count++</button>
+        <div>
+            <div className="d-flex">
+                <span>{count}</span>
+                <button 
+                    type="button" 
+                    className="btn-primary mx-2"
+                    onClick={() => setCount(1)}>
+                        Count++
+                </button>
+            </div>
+            <div key="check">
+                <input
+                    id="check" 
+                    type='checkbox'
+                    checked={isCheck}
+                    onChange={toggle}
+                >
+                </input>
+                <label htmlFor="check" className="mx-2">
+                    {isCheck ? "Checked" : "Not Checked"}
+                </label>
+            </div>
         </div>
     )
 }
