29e72d3766ae596f11e627428cb777c16ac4c243
jeffliang0318 <liangnash0318@gmail.com>
2021-01-27 15:11:50 -0800

    Play useReducer with dispatch and action

--- src/useReducer.tsx
+++ src/useReducer.tsx
@@ -1,8 +1,30 @@
 import { useReducer } from 'react';
+interface Message {
+    message: string
+}
+
+interface Action {
+    type: string
+}
+const initialState: Message = {
+    message: 'hi'
+}
+
+function reducer (state: Message, action: Action): Message {
+    switch (action.type) {
+        case 'YELL':
+            return { message: `HEY! I said ${state.message}` };
+        case 'whisper':
+            return { message: `excuse me~ I said ${state.message}` };
+        default:
+            return { message: 'hi' };
+    }
+}
 
 export default function CaseUseReducer () {
     const [count, setCount] = useReducer((count: number, newCount: number) => { return count + newCount}, 0)
     const [isCheck, toggle] = useReducer((isCheck: boolean) =>  !isCheck, false)
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <div>
             <div className="d-flex">
@@ -25,6 +47,30 @@ export default function CaseUseReducer () {
                 <label htmlFor="check" className="mx-2">
                     {isCheck ? "Checked" : "Not Checked"}
                 </label>
+            </div>
+            <div>
+                {state.message}
+                <button 
+                    type='button' 
+                    className="btn-primar mx-2"
+                    onClick={() => dispatch({type: "YELL"})}
+                >
+                    YELL
+                </button>
+                <button 
+                    type='button' 
+                    className="btn-primar mx-2"
+                    onClick={() => dispatch({type: "whisper"})}
+                >
+                    whisper
+                </button>
+                <button 
+                    type='button' 
+                    className="btn-primar mx-2"
+                    onClick={() => dispatch({type: "hi"})}
+                >
+                    hi
+                </button>
             </div>
         </div>
     )
