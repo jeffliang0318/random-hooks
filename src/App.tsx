61bc41c7e62068b4d98c086bf168f3a72257367b
jeffliang0318 <liangnash0318@gmail.com>
2021-02-03 23:08:58 -0800

    Add bootstrap custom variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,36 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import './App.css';
-import Head from './head'
-import RandomUser from './random-user'
-import Home from './Home'
-import Star from './star'
-import Reference from './reference'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import Head from "./head";
+import RandomUser from "./random-user";
+import Home from "./Home";
+import Star from "./star";
+import Reference from "./reference";
+import StyleGuide from "./styleguide";
 
 function App() {
-
   return (
     <Router>
       <Head></Head>
       <Switch>
-          <Route exact path="/">
-            <Home></Home>
-            <RandomUser />
-          </Route>
-          <Route path="/random-user">
-            <RandomUser />
-          </Route>
-          <Route path="/star">
-            <Star/>
-          </Route>
-          <Route path="/ref">
-            <Reference />
-          </Route>
-          <Route path="/styleguide">
-            {/* <Reference /> */}
-          </Route>
-        </Switch>
+        <Route exact path="/">
+          <Home></Home>
+          <RandomUser />
+        </Route>
+        <Route path="/random-user">
+          <RandomUser />
+        </Route>
+        <Route path="/star">
+          <Star />
+        </Route>
+        <Route path="/ref">
+          <Reference />
+        </Route>
+        <Route path="/styleguide">
+          <StyleGuide />
+        </Route>
+      </Switch>
     </Router>
   );
 }
-
 
 export default App;
--- src/head.tsx
+++ src/head.tsx
@@ -1,24 +1,47 @@
 // import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
-export default function Nav () {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-            <div className='container-md'>
-                <Link className="navbar-brand" to="/">Brand</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link className="nav-item nav-link" to="/">Home</Link>
-                        <Link className="nav-item nav-link" to="/random-user">Random User</Link>
-                        <Link className="nav-item nav-link" to="/ref">Ref, callback, reducer</Link>
-                        <Link className="nav-item nav-link" to="/star">Star</Link>
-                        <Link className="nav-item nav-link disabled" to="/">Disabled</Link>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+export default function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <div className="container-md">
+        <Link className="navbar-brand" to="/">
+          Brand
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-item nav-link" to="/random-user">
+              Random User
+            </Link>
+            <Link className="nav-item nav-link" to="/ref">
+              Ref, callback, reducer
+            </Link>
+            <Link className="nav-item nav-link" to="/star">
+              Star
+            </Link>
+            <Link className="nav-item nav-link" to="/styleguide">
+              Style Guide
+            </Link>
+            <Link className="nav-item nav-link disabled" to="/">
+              Disabled
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
